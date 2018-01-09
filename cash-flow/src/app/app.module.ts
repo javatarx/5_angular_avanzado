0496a7899f01b094b0541d3cb8a7265ccff6f6b7
Alberto Basalo <albertobasalo@hotmail.com>
2018-01-09 16:56:17 +0100

    app.routing rename

--- cash-flow/src/app/app.module.ts
+++ cash-flow/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routing';
 
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
