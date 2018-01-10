2c3bfbb7ea49da87ac6b15896a8d9404eae15036
Alberto Basalo <albertobasalo@hotmail.com>
2018-01-10 20:40:45 +0100

    operations cli

--- cash-flow/src/app/views/operations/operations.module.ts
+++ cash-flow/src/app/views/operations/operations.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { OperationsRoutingModule } from './operations.routing';
 import { OperationsComponent } from './operations.component';
+import { NewComponent } from './new/new.component';
+import { ListComponent } from './list/list.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   imports: [
     CommonModule,
     OperationsRoutingModule
   ],
-  declarations: [OperationsComponent]
+  declarations: [OperationsComponent, NewComponent, ListComponent, ItemComponent]
 })
 export class OperationsModule { }
