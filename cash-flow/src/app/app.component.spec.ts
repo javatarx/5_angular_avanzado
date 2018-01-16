4991ce5167bf44abe59cee8c13675885b0838ade
Alberto Basalo <albertobasalo@agorabinaria.com>
2018-01-16 12:54:08 +0100

    app spec module configuration

--- cash-flow/src/app/app.component.spec.ts
+++ cash-flow/src/app/app.component.spec.ts
@@ -1,15 +1,21 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ComponentsModule } from './lib/components/components.module';
+import { BusService } from './lib/bus.service';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        ComponentsModule,
         RouterTestingModule
       ],
       declarations: [
         AppComponent
       ],
+      providers: [
+        BusService
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
