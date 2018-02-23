9d5e871d40f5a8cf5221440dc53cd3abe5e9b83c
Alberto Basalo <albertobasalo@hotmail.com>
2018-02-23 17:07:07 +0100

    import jasmine

--- cash-flow/src/app/app.component.spec.ts
+++ cash-flow/src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import {} from 'jasmine';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './lib/components/components.module';
 import { StoreService } from './lib/store.service';
