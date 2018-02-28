079ba58e2131c602a19496d6f108fc9c60900d8b
Alberto Basalo <albertobasalo@hotmail.com>
2018-02-28 18:41:38 +0100

    fix app modules

--- cash-flow/src/app/app.module.ts
+++ cash-flow/src/app/app.module.ts
@@ -1,24 +1,26 @@
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app.routing';
-import { BrowserModule } from '@angular/platform-browser';
-import { environment } from '../environments/environment';
-import { NgModule } from '@angular/core';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { NotFoundModule } from './views/not-found/not-found.module';
-import { LibModule } from './lib/lib.module';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment";
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app.routing";
+import { LibModule } from "./lib/lib.module";
+import { HomeModule } from "./views/home/home.module";
+import { NotFoundModule } from "./views/not-found/not-found.module";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [
-    AppRoutingModule,
-    BrowserModule,
-    LibModule,
-    NotFoundModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', {
-      enabled: environment.production
-    })
-  ],
-  bootstrap: [AppComponent],
-  exports: []
+	declarations: [AppComponent],
+	imports: [
+		AppRoutingModule,
+		BrowserModule,
+		HomeModule,
+		LibModule,
+		NotFoundModule,
+		ServiceWorkerModule.register("/ngsw-worker.js", {
+			enabled: environment.production
+		})
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule {}
